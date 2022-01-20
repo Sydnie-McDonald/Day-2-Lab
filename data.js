b6bf4a876c63e6da8717af3fc05c943f0948cfe9
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-19 18:43:58 -0600

    added to data.js created characters

---
+++ data.js
@@ -0,0 +1,30 @@
+export const people = [
+    {
+        id: 1,
+        name: 'Christina',
+        age: 42,
+        bio: 'I am your person Meredith.',
+        hobbies: ['motorcycle', 'studying', 'cardio-surgery'],
+    },
+    {
+        id: 2,
+        name: 'Meredith',
+        age: 40,
+        bio: 'crying, being dark and twisty, finding lost siblings, raising children',
+        hobbies: ['winning awards', 'death', 'ruining relationships'],
+    },
+    {
+        id: 3,
+        name: 'Izzy',
+        age: 32,
+        bio: 'crazy spunky loves hard had brain tumor ',
+        hobbies: ['baking', 'model', 'day dreaming'],
+    },
+    {
+        id: 4,
+        name: 'Alex',
+        age: 28,
+        bio: 'very talented, great with kids, not great with girlfriends, kinda mean but big heart.',
+        hobbies: ['saving lives', 'confidant', 'mentor'],
+    },
+];
