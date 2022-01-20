4f9e3973431a7d941e1b38dc35f2ffecc48ef20d
aidanliddiard <aidanliddiard@gmail.com>
2022-01-20 12:34:56 -0800

    debugged renderPerson function

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const nameEl = document.querySelector('.name');
 const ageEl = document.querySelector('.age');
 const bioEl = document.querySelector('.bio');
 const hobbiesEl = document.querySelector('.hobbies');
-const containerEl = document.querySelector('.container');
+const containerEl = document.querySelector('.contain');
 console.log(selectEl, nameEl, ageEl, bioEl, hobbiesEl, containerEl);
 
 
 function renderPerson(index) {
-    contain.classList.add(`${people[index].name}`);
+    containerEl.classList.add(`${people[index].name}`);
     nameEl.textContent = 'Name: ' + people[index].name;
     ageEl.textContent = 'Age: ' + people[index].age;
     bioEl.textContent = 'Bio: ' + people[index].bio;
-    
+
     for (let hobby of people[index].hobbies) {
         const li = document.createElement('li');
         li.textContent = hobby;
@@ -24,6 +24,7 @@ function renderPerson(index) {
     }
 }
 
+renderPerson(0);
 // set event listeners
 selectEl.addEventListener('change', (e) => {
     // console.log('user click');
