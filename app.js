5c314fccb3022f606d3a33527b940435973f59ac
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-20 14:12:07 -0600

    added to app.js, import data.js, and accessing the elements for the characters/ people

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // import functions
-
+import { people } from './data.js'; 
+console.log(people, 'people');
 // grab DOM elements
+const selectEl = document.querySelector('.select');
+const nameEl = document.querySelector('.name');
+const ageEl = document.querySelector('.age');
+const bioEl = document.querySelector('.bio');
+const hobbiesEl = document.querySelector('.hobbies');
+const containerEl = document.querySelector('.container');
+console.log(selectEl, nameEl, ageEl, bioEl, hobbiesEl, containerEl);
+
+
+
 
 // set event listeners 
     // get user input
